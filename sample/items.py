cdaab26d4c454848623c229c34d1c4d2ac228b8c
Diego Velásquez Ríos <diego@MacBook-Pro-de-Diego.local>
2017-07-26 01:26:10 -0500

    first commit

---
+++ sample/items.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+import scrapy
+
+
+class SampleItem(scrapy.Item):
+    pass
+
---
+++ sample/settings.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+
+BOT_NAME = 'sample'
+
+SPIDER_MODULES = ['sample.spiders']
+NEWSPIDER_MODULE = 'sample.spiders'
+
+# Obey robots.txt rules
+ROBOTSTXT_OBEY = True
+
+# Enable or disable spider middlewares
+# See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
+#SPIDER_MIDDLEWARES = {
+#    'tutorial.middlewares.TutorialSpiderMiddleware': 543,
+#}
+
+# Enable or disable downloader middlewares
+# See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
+#DOWNLOADER_MIDDLEWARES = {
+#    'tutorial.middlewares.MyCustomDownloaderMiddleware': 543,
+#}
+
+# Enable or disable extensions
+# See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
+#EXTENSIONS = {
+#    'scrapy.extensions.telnet.TelnetConsole': None,
+#}
+
+# Configure item pipelines
+# See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
+#ITEM_PIPELINES = {
+#    'tutorial.pipelines.TutorialPipeline': 300,
+#}
